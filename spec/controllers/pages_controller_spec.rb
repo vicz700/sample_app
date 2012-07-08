45200e62571870cefc4098c14f68b8d19d3644f4
vic ross <vicz700@hotmail.com>
2012-07-08 13:24:01 -0400

    Done with static pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,6 +3,12 @@ require 'spec_helper'
 describe PagesController do
   render_views
 
+  before(:each) do
+
+    @base_title = "Ruby on Rails Tutorial Sample App | #{@title}"
+
+  end
+
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
@@ -11,8 +17,7 @@ describe PagesController do
 
     it "should have the right title" do
       get 'home'
-      response.should have_selector("title",
-                                    :content => "Ruby on Rails Tutorial Sample App | Home")
+      response.should have_selector("title", :content => @base_title)
     end
 
     it "should have a non- blank body" do
@@ -30,8 +35,7 @@ describe PagesController do
 
     it "should have the right title" do
       get 'contact'
-      response.should have_selector("title",
-                                    :content => "Ruby on Rails Tutorial Sample App | Contact")
+      response.should have_selector("title", :content => @base_title)
     end
   end
 
@@ -43,8 +47,19 @@ describe PagesController do
 
     it "should have the right title" do
       get 'about'
-      response.should have_selector("title",
-                                    :content => "Ruby on Rails Tutorial Sample App | About")
+      response.should have_selector("title", :content => @base_title)
+    end
+  end
+
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'help'
+      response.should have_selector("title", :content => @base_title)
     end
   end
 
