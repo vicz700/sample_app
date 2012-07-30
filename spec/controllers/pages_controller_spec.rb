df12fba1eb1df52f4c6f9ac7abcfeb804b2180ab
vic ross <vicz700@hotmail.com>
2012-07-29 21:49:07 -0400

    Removed default Rails page

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -17,7 +17,7 @@ describe PagesController do
 
     it "should have the right title" do
       get 'home'
-      response.should have_selector("title", :content => @base_title + " | Home")
+      response.should have_selector("title", :content => @base_title )
     end
 
     it "should have a non- blank body" do
